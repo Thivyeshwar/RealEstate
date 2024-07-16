85c592cbad696f760005e522266e22ac2fe228f9
THIVYESHWAR SIVA KUMAR <thivyeshwarsivakumar@THIVYESHWARs-MacBook-Air.local>
2024-07-16 11:02:23 +0530

    added 2 sections

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import Companies from './component/companies/Companies';
+import Contact from './component/Contact/Contact';
 import Extra from './component/Extra/Extra';
 import Header from './component/Header/Header';
 import Residencies from "./component/residencies/Residencies"
+import Value from './component/value/Value';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       </div>
       <Companies/>
       <Residencies/>
+      <Value/>
+      <Contact/>
     </div>
   );
 }
